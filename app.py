1a009e3e0e2faf5a4cd4bcbfe43b031e5057af16
tomasGk <thomas.gkotsis@gmail.com>
2018-04-04 16:22:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Python!"
+    return "Hello from dockerized flask!"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 6000))
+    app.run(port=port)
